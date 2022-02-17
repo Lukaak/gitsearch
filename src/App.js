e3d1589e78e9682f4c608ea4a5a219976e5ccf9c
luka kandaurishvili <lukakandauri@gmail.com>
2022-02-17 13:44:23 +0400

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect,useState} from "react";
+import './App.css'
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data,setData] = useState([])
+    const [searchWord,setSearchWord] = useState('')
+    const [suggestion,setSuggestion] = useState([])
+
+    useEffect(()=>{
+        const getUsers = () =>{
+            let url = `https://api.github.com/users`
+            fetch(url).then(res => res.json()).then(
+                data => {
+                    setData(data)
+                }).catch(e=>{
+                    console.log('data not found')
+                })
+
+        }
+        getUsers()
+    },[])
+
+    const handlechange = (word) => {
+        let matches = []
+        if(word.length>0){
+            matches = data.filter(user=>{
+                const regex = new RegExp(`${word}`,"gi");
+                return user.login.match(regex)
+            })
+        }
+
+        setSuggestion(matches)
+        setSearchWord(word)
+        
+    }
+
+
+
+    return(
+        <div className="container">
+            <div className="inputContainer">  
+                <p>search github users here:</p>
+                <input onChange={e =>handlechange(e.target.value)} className="inputField" type="text" placeholder="Git User" ></input>
+            </div>
+            <div className="contentContainer">
+                    <div className="contentChildContainer">
+                        {searchWord==''?
+                        (data.map((val,key)=>
+                        {return <a key={val.id} className="link" target="_blank" href={val.html_url}>
+                                <div className="userBox">
+                                    <div className="ProfilePic">
+                                        <img src={val.avatar_url} alt="Girl in a jacket" width="298" height="230"></img>
+                                    </div>
+                                    <div className="UserName">
+                                        {val.login}
+                                    </div>
+                                </div>
+                                </a>}
+                        )):(suggestion.map((val,keyl)=>
+                        {return <a key={val.id} className="link" target="_blank" href={val.html_url}>
+                                <div className="userBox">
+                                    <div className="ProfilePic">
+                                        <img src={val.avatar_url} alt="Girl in a jacket" width="298" height="230"></img>
+                                    </div>
+                                     <div className="UserName">
+                                        {val.login}
+                                    </div>
+                                </div>
+                                </a>}
+                                
+                        ))}
+                    </div>
+                </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
